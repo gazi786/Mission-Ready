3edb031cfcbfc0fa1503191fcfe8a4d9f8125c7d
Alshafaraz Gazi <alshafaraz.gazi@gmail.com>
2023-01-26 15:31:27 +1300

    Week 2 Day 4

---
+++ InSession/js/learningjs.js
@@ -0,0 +1,5 @@
+const firstAndLastName = "Alshafaraz Gazi";
+const age = 31;
+const ageCalc = 2023 - 1991;
+
+console.log(firstAndLastName + " is " + age + " years old. However, he will be " + ageCalc + " years old in 2023.");
